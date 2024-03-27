5467ef13e2d9d8de3729b30341a160c82cf27e59
mmww111161 <melwright61@virginmedia.com>
2024-03-27 21:11:20 +0000

    changed to postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        # 'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'django',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': "127.0.0.1",
+        'PORT': 5432,
+        'USER': "postgres",
+        'PASSWORD': 'letmein'
+    },
 }
 
 
